f474a3461f3d395089cf8e50d6af486ce24f6fab
Matt Morgan <mmorgan@jamasoftware.com>
2017-08-21 18:29:44 -0700

    dockerize tiernan and refactor some things.

--- run_sslscan.py
+++ run_sslscan.py
@@ -1,16 +1,11 @@
-#!/usr/bin/env python
 import subprocess
 
 
 def scan(target):
     scan_result = subprocess.run(
-        ["docker", "run", "--rm", "sslscan", "--no-colour", target],
+        ["/usr/bin/sslscan", target],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         universal_newlines=True
     )
     return scan_result.stdout
-
-
-if __name__ == "__main__":
-    print(scan("qaappvault.jamasoftware.net"))
--- tiernan.py
+++ tiernan.py
@@ -1,19 +1,14 @@
+#!/usr/bin/python3
 from run_sslscan import scan
-from flask import Flask, request, session
+from flask import Flask, request, Response
 from ansi2html import Ansi2HTMLConverter
-import time
-import os
-from datetime import timedelta
 
 app = Flask(__name__)
-app.permanent_session_lifetime = timedelta(seconds=30)
-app.secret_key = os.urandom(24)
 ansi_converter = Ansi2HTMLConverter()
 
 
 @app.route("/", methods=['GET'])
 def get_scan():
-    session.permanent = True
     request_form = """
     <form action="/" method="post">
     Site to scan: <input type="text" name="target_site"><br>
@@ -26,9 +21,18 @@ def get_scan():
 
 @app.route("/", methods=['POST'])
 def run_scan():
-    print(time.time())
     target = request.form.get('target_site')
-    scan_results = scan(target)
-    html = ansi_converter.convert(scan_results)
-    print(time.time())
-    return html
+
+    def generate():
+        yield "<br/>"
+        # This generate sends something back to the browser to avoid timeouts on the POST.
+        # If the time bought by the single <br/> were to be exceeded this would need to be rewritten.
+        # if subprocess Popen were used instead of run then you could stream back data while waiting on the scan.
+        # This caused me a problem AFTER I dockerized sslscan and might have "gone away" now that it's all dockerized.
+        scan_results = scan(target)
+        html = ansi_converter.convert(scan_results)
+        yield html
+    return Response(generate())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
