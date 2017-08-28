4d0e967344139934befe68eadfcf79fb678584a7
Matt Morgan <mmorgan@jamasoftware.com>
2017-08-28 15:17:54 -0700

    - moved from flask to cherrypy

--- tiernan.py
+++ tiernan.py
@@ -1,40 +1,28 @@
 #!/usr/bin/python3
 from run_sslscan import scan
-from flask import Flask, request, Response
+import cherrypy
 from ansi2html import Ansi2HTMLConverter
-from werkzeug.serving import WSGIRequestHandler
 
-app = Flask(__name__)
 ansi_converter = Ansi2HTMLConverter()
+cherrypy.config.update({'server.socket_host': '0.0.0.0'})
 
-WSGIRequestHandler.protocol_version = "HTTP/1.1"
+class Tiernan(object):
+    @cherrypy.expose
+    def index(self):
+        request_form = """
+        <form action="/doScan" method="get">
+        Site to scan: <input type="text" name="target_site" size="50"><br>
+        <input type="submit" value="Submit">
+        </form>
 
-@app.route("/", methods=['GET'])
-def get_scan():
-    request_form = """
-    <form action="/" method="post">
-    Site to scan: <input type="text" name="target_site" size="50"><br>
-    <input type="submit" value="Submit">
-    </form>
+        """
+        return request_form
 
-    """
-    return request_form
-
-
-@app.route("/", methods=['POST'])
-def run_scan():
-    target = request.form.get('target_site')
-
-    def generate():
-        yield "<br/>"
-        # This generate sends something back to the browser to avoid timeouts on the POST.
-        # If the time bought by the single <br/> were to be exceeded this would need to be rewritten.
-        # if subprocess Popen were used instead of run then you could stream back data while waiting on the scan.
-        # This caused me a problem AFTER I dockerized sslscan and might have "gone away" now that it's all dockerized.
-        scan_results = scan(target)
+    @cherrypy.expose
+    def doScan(self, target_site):
+        scan_results = scan(target_site)
         html = ansi_converter.convert(scan_results)
-        yield html
-    return Response(generate())
+        return html
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    cherrypy.quickstart(Tiernan())
